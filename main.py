255a6a3039307583ddc2673ab46ee15b98f596fd
Tomoki Morimoto <morimoto.ke@gmail.com>
2021-12-19 18:11:37 +0900

    ✨ バリデーション（クエリパラメータと文字列の検証）

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 # リクエストボディ
 """
@@ -34,7 +34,7 @@ Pythonの型宣言だけでFast APIは以下のことを実施する。
 - モデルの JSON スキーマ定義を生成し、好きな場所で使用することができます。
 - これらのスキーマは、生成されたOpen APIスキーマの一部となり、自動ドキュメントのUIに使用されます。
 """
-@app.post("/items/")
+@app.post("/items1/")
 async def create_item(item: Item):
     # 関数内部でモデルの全ての属性に直接アクセスできる。
     item_dict = item.dict()
@@ -48,7 +48,7 @@ async def create_item(item: Item):
 FastAPIはパスパラメータである関数パラメータはパスから受け取り、Pydanticモデルによって宣言された関数パラメータはリクエストボディから受け取るということを認識します。
 
 """
-@app.put("/items/{item_id}")
+@app.put("/items2/{item_id}")
 async def create_item(item_id: int, item: Item):
     return {"item_id": item_id, **item.dict()}
 
@@ -57,7 +57,7 @@ async def create_item(item_id: int, item: Item):
 また、ボディとパスとクエリのパラメータも同時に宣言できます。
 FastAPIはそれぞれを認識し、適切な場所からデータを取得します。
 """
-@app.put("items/{item_id}")
+@app.put("/items3/{item_id}")
 async def create_item(item_id: int, item: Item, q:Optional[str] = None):
     result = {"item_id": item_id, **item.dict()}
     if q:
@@ -69,3 +69,19 @@ async def create_item(item_id: int, item: Item, q:Optional[str] = None):
 - パラメータが単数型(int, float, str, bool など)の場合はクエリパラメータとして解釈されます。
 - パラメータがPydanticモデル型で宣言された場合、リクエストボディとして解釈されます。
 """
+
+# バリデーション（クエリパラメータと文字列の検証）
+"""
+1. バリデーションの追加：FastAPIからQueryをインポートする。
+from fastapi import Query
+2. パラメータのデフォルト値として使用し、パラメータのmax_lengthを50に設定する。
+3. min_lemgthも追加する
+4. 正規表現を設定する
+5. 必須にする場合はQueryの第一引数をNoneから ... に変更する
+"""
+@app.get("/items4")
+async def read_items(q: Optional[str] = Query(..., max_length=50, min_length=3, regex="^fixedquery$")):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({q: q})
+    return results
